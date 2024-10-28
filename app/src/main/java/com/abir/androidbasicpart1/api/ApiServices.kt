dbd5b6513a6c54078b297b149dce08781f1cf9f2
abir4u <abirpal90@gmail.com>
2024-10-28 16:53:50 +1300

    Refactor APIServices and UserViewModel

--- app/src/main/java/com/abir/androidbasicpart1/api/ApiServices.kt
+++ app/src/main/java/com/abir/androidbasicpart1/api/ApiServices.kt
@@ -1,17 +1,20 @@
 package com.abir.androidbasicpart1.api
 
-import com.abir.androidbasicpart1.data.User
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 
-data class UserRequest(val user_id: String)
+data class UserRequest(
+    val user_Id: String
+)
 
 interface ApiServices {
     @GET("/get-users")
-    suspend fun getUsers(): Response<List<User>>
+    suspend fun getUsers(): Response<Map<String, Any>>
 
     @POST("new-user-conversation")
-    suspend fun newUserConversation(@Body request: UserRequest): Response<Map<String, String>>
+    suspend fun newUserConversation(
+        @Body request: UserRequest
+    ): Response<Map<String, Any>>
 }
--- app/src/main/java/com/abir/androidbasicpart1/viewmodels/UserViewModel.kt
+++ app/src/main/java/com/abir/androidbasicpart1/viewmodels/UserViewModel.kt
@@ -10,28 +10,33 @@ import com.abir.androidbasicpart1.api.RetrofitInstance
 import com.abir.androidbasicpart1.api.UserRequest
 import kotlinx.coroutines.launch
 import retrofit2.HttpException
+import retrofit2.Response
 import java.io.IOException
 
 class UserViewModel : ViewModel() {
 
-    var responseBody by mutableStateOf<List<Any>>(emptyList())
+    var responseBody by mutableStateOf<Map<String, Any>>(emptyMap())
     private var errorMessage by mutableStateOf("")
     var code by mutableIntStateOf(0)
     var responseHeaders by mutableStateOf<Map<String, List<String>>>(emptyMap()) // Map for headers
     var cookies by mutableStateOf<List<String>>(emptyList()) // List for cookies
     private var isSuccess by mutableStateOf(true)
 
+    private fun populateMandatoryParams(response: Response<Map<String, Any>>) {
+        code = response.code()
+        // Extract headers and store them in responseHeaders
+        responseHeaders = response.headers().toMultimap()
+        // Extract cookies if any are present in the headers
+        cookies = response.headers()["Set-Cookie"]?.let { listOf(it) } ?: emptyList()
+    }
+
     fun getUsers() {
         viewModelScope.launch {
             try {
                 val response = RetrofitInstance.api.getUsers()
                 isSuccess = response.isSuccessful
-                responseBody = response.body() ?: emptyList()
-                code = response.code()
-                // Extract headers and store them in responseHeaders
-                responseHeaders = response.headers().toMultimap()
-                // Extract cookies if any are present in the headers
-                cookies = response.headers()["Set-Cookie"]?.let { listOf(it) } ?: emptyList()
+                responseBody = response.body() ?: emptyMap()
+                populateMandatoryParams(response)
             } catch (e: IOException) {
                 errorMessage = "Network Error: ${e.message}"
             } catch (e: HttpException) {
@@ -43,23 +48,19 @@ class UserViewModel : ViewModel() {
     fun newUserConversation(userId: String) {
         viewModelScope.launch {
             try {
-                val requestBody = UserRequest(user_id = userId)
+                val requestBody = UserRequest(user_Id = userId)
                 val response = RetrofitInstance.api.newUserConversation(requestBody)
 
                 if (response.isSuccessful && response.body() != null) {
                     isSuccess = true
                     // Extract message or status from response
-                    responseBody = listOf(response.body() ?: emptyMap())
                 } else {
                     isSuccess = false
                     // Handle unsuccessful response
-                    responseBody = listOf("Failed to create conversation: ${response.message()}")
+                    errorMessage = "Failed to create conversation: ${response.message()}"
                 }
-                code = response.code()
-                // Extract headers and store them in responseHeaders
-                responseHeaders = response.headers().toMultimap()
-                // Extract cookies if any are present in the headers
-                cookies = response.headers()["Set-Cookie"]?.let { listOf(it) } ?: emptyList()
+                responseBody = response.body() ?: emptyMap()
+                populateMandatoryParams(response)
             } catch (e: Exception) {
                 errorMessage = "Network error: ${e.message}"
             }
