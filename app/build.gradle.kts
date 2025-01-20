9cb131c564c2ff6c15fab67ffab60db7d2711fdb
abir4u <abirpal90@gmail.com>
2025-01-20 17:38:00 +1300

    Upgrade dependencies to their latest versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 android {
     namespace = "com.abir.androidbasicpart1"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.abir.androidbasicpart1"
