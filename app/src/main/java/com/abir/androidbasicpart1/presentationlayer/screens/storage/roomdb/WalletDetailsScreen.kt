70e9e857a36c6c14e0e40877f216637cec2f28ea
abir4u <abirpal90@gmail.com>
2025-01-24 22:38:13 +1300

    Remove unused imports in Wallet functionality

--- app/src/main/java/com/abir/androidbasicpart1/presentationlayer/screens/storage/roomdb/WalletDetailsScreen.kt
+++ app/src/main/java/com/abir/androidbasicpart1/presentationlayer/screens/storage/roomdb/WalletDetailsScreen.kt
@@ -1,12 +1,10 @@
 package com.abir.androidbasicpart1.presentationlayer.screens.storage.roomdb
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.abir.androidbasicpart1.datalayer.models.WalletItem
